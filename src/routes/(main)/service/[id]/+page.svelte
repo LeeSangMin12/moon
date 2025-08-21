71a62061db09f70ef5cdfc14a9b9e075107adb0c
이상민 <isangmin516@gmail.com>
2025-08-21 10:30:43 +0900

    service iframe 대신 설명으로 변경

--- src/routes/(main)/service/[id]/+page.svelte
+++ src/routes/(main)/service/[id]/+page.svelte
@@ -446,18 +446,18 @@
 		<p class="text-primary mt-6 text-xl font-bold">₩{comma(service.price)}</p>
 
 		<!-- Service Description -->
-		<!-- <div class="mt-4">
+		<div class="mt-4">
 			<div class="min-h-[184px] w-full rounded-[7px] bg-gray-50 px-5 py-4">
 				<div class="text-sm whitespace-pre-wrap">{service.content}</div>
 			</div>
-		</div> -->
-		<iframe
+		</div>
+		<!-- <iframe
 			title="service_description"
 			src={service.content}
 			style="width: 100%; height: 500px; border: none !important; padding: 0"
 			frameborder="0"
 			allowfullscreen
-		></iframe>
+		></iframe> -->
 
 		<!-- Reviews Section -->
 		{#key service_reviews.length + (my_review?.id || 0)}
