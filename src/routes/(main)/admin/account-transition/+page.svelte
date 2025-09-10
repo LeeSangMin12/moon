a53f3887607a63ed65e8cf4329fc027e8e012932
이상민 <isangmin516@gmail.com>
2025-09-10 13:45:19 +0900

    가상 유저 생성

--- src/routes/(main)/admin/account-transition/+page.svelte
+++ src/routes/(main)/admin/account-transition/+page.svelte
@@ -1,7 +1,7 @@
 <script>
+	import { smartGoBack } from '$lib/utils/navigation';
 	import { goto } from '$app/navigation';
 	import { RiArrowLeftSLine } from 'svelte-remixicon';
-	import { smartGoBack } from '$lib/utils/navigation';
 
 	import Header from '$lib/components/ui/Header/+page.svelte';
 
@@ -39,15 +39,45 @@
 	</button>
 
 	<button
+		onclick={() => sign_in('dummy4@naver.com', '1234')}
+		class="btn btn-primary"
+		>매일 Ai 뉴스봇
+	</button>
+
+	<button
+		onclick={() => sign_in('dummy7@naver.com', '1234')}
+		class="btn btn-primary"
+		>김윤지
+	</button>
+
+	<button
+		onclick={() => sign_in('dummy8@naver.com', '1234')}
+		class="btn btn-primary"
+		>박서윤
+	</button>
+
+	<button
+		onclick={() => sign_in('dummy9@naver.com', '1234')}
+		class="btn btn-primary"
+		>김도현
+	</button>
+
+	<button
+		onclick={() => sign_in('dummy10@naver.com', '1234')}
+		class="btn btn-primary"
+		>이준호
+	</button>
+
+	<button
 		onclick={() => sign_in('dummy5@naver.com', '1234')}
 		class="btn btn-primary"
 		>데이터먹는쩡은
 	</button>
 
 	<button
-		onclick={() => sign_in('dummy4@naver.com', '1234')}
+		onclick={() => sign_in('dummy2@naver.com', '1234')}
 		class="btn btn-primary"
-		>매일 Ai 뉴스봇
+		>디자인 크리에이터 유민
 	</button>
 
 	<button
@@ -55,11 +85,7 @@
 		class="btn btn-primary"
 		>모던랩 스튜디오 - Plan · Design · Code
 	</button>
-	<button
-		onclick={() => sign_in('dummy2@naver.com', '1234')}
-		class="btn btn-primary"
-		>디자인 크리에이터 유민
-	</button>
+
 	<button
 		onclick={() => sign_in('dummy1@naver.com', '1234')}
 		class="btn btn-primary"
