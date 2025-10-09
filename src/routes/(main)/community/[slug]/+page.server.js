16def4f85b4e9f5629dad7298889c49e3ac01d9b
이상민 <isangmin516@gmail.com>
2025-10-09 18:21:19 +0900

    커뮤니티 참여 안되는 오류 수정

--- src/routes/(main)/community/[slug]/+page.server.js
+++ src/routes/(main)/community/[slug]/+page.server.js
@@ -12,7 +12,7 @@ export async function load({ params, parent, locals: { supabase }, setHeaders })
 	const community = await api.communities.select_by_slug(params.slug);
 
 	const [community_members, posts, community_participants] = await Promise.all([
-		user ? api.community_members.select_by_user_id(user.id) : Promise.resolve([]),
+		user?.id ? api.community_members.select_by_user_id(user.id) : Promise.resolve([]),
 		api.posts.select_by_community_id(community.id, 10),
 		api.community_members.select_by_community_id(community.id)
 	]);
