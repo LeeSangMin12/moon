54562b0742b706a268790f7d4ed9cab8e3d43789
이상민 <isangmin516@gmail.com>
2025-10-18 21:05:47 +0900

    배너 및 이벤트 페이지 완성

--- migration_add_payment_info.sql
+++
@@ -1,9 +0,0 @@
--- 입금 정보를 저장할 JSON 컬럼 추가
-
-ALTER TABLE expert_requests
-ADD COLUMN IF NOT EXISTS payment_info JSONB;
-
-COMMENT ON COLUMN expert_requests.payment_info IS '입금 정보 (depositor_name, bank, account_number 등)';
-
--- 완료
-SELECT 'Payment info column added!' as status;
--- migration_add_reject_reason_to_expert_requests.sql
+++
@@ -1,5 +0,0 @@
--- Add reject_reason column to expert_requests table
-ALTER TABLE expert_requests
-ADD COLUMN IF NOT EXISTS reject_reason TEXT;
-
-COMMENT ON COLUMN expert_requests.reject_reason IS '관리자가 공고를 거절한 경우 사유';
