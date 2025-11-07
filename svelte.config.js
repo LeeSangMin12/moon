50f22a670564288099fc6dd08fbd9fd3abbd32a3
이상민 <isangmin516@gmail.com>
2025-11-08 02:28:53 +0900

    패키지 정리

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,4 @@ export default {
 	kit: {
 		adapter: adapter({}),
 	},
-	// svelte-carousel 사용시 필요
-	vite: {
-		optimizeDeps: {
-			include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep'],
-		},
-	},
 };
